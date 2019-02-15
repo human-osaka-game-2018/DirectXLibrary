1eda694096b70ec3f81b079c1993b40c9246fd26
Harutaka-Tsujino <glaca986@outlook.jp>
2019-02-16 01:27:00 +0900

    [issue #1] エフェクトマネージャをアプリよりだったためライブラリ寄りに修正

--- DirectXLibrary/GameLib/EffectManager/EffectManager.cpp
+++ DirectXLibrary/GameLib/EffectManager/EffectManager.cpp
@@ -1,1 +1,75 @@
-﻿
+﻿/// <filename>
+/// EffectManager.cpp
+/// </filename>
+/// <summary>
+/// エフェクト管理クラスのソース
+/// </summary>
+
+#include "EffectManager.h"
+
+#include <Windows.h>
+
+#include <vector>
+
+#include <d3dx9.h>
+
+#include "Effect\Effect.h"
+#include "Effect\Effects\Enum\EFFECT_ID.h"
+#include "Effect\Effects\Effects.h"
+
+void EffectManager::AddEffect(Effect* pEffect)
+{
+	m_pEffects.push_back(pEffect);
+}
+
+void EffectManager::Update()
+{
+	if (m_pEffects.size() <= 0) return;
+
+	ReleaseEndEffects();
+
+	for (auto i : m_pEffects)
+	{
+		i->Update();
+	}
+}
+
+void EffectManager::Render()
+{
+	if (m_pEffects.size() <= 0) return;
+
+	for (auto i : m_pEffects)
+	{
+		i->Render();
+	}
+}
+
+void EffectManager::AllRelease()
+{
+	for (auto i : m_pEffects)
+	{
+		delete i;
+	}
+
+	m_pEffects.clear();
+	m_pEffects.shrink_to_fit();
+}
+
+void EffectManager::ReleaseEndEffects()
+{
+	if (m_pEffects.size() <= 0) return;
+
+	for (int i = static_cast<int>(m_pEffects.size() - 1); i >= 0; --i)
+	{
+		if (!m_pEffects[i]->GetEnds())
+		{
+			continue;
+		}
+
+		delete m_pEffects[i];
+
+		std::vector<Effect*>::iterator thisIndexItr = m_pEffects.begin() + i;
+
+		m_pEffects.erase(thisIndexItr);
+	}
+}
--- DirectXLibrary/GameLib/EffectManager/EffectManager.h
+++ DirectXLibrary/GameLib/EffectManager/EffectManager.h
@@ -1,4 +1,11 @@
-﻿#ifndef EFFECT_MANAGER_H
+﻿/// <filename>
+/// EffectManager.h
+/// </filename>
+/// <summary>
+/// エフェクト管理クラスのヘッダ
+/// </summary>
+
+#ifndef EFFECT_MANAGER_H
 #define EFFECT_MANAGER_H
 
 #include <Windows.h>
@@ -6,108 +13,50 @@
 #include <vector>
 
 #include <d3dx9.h>
-
-#include "../Class/Singleton/Singleton.h"
+	
 #include "Effect\Effect.h"
 #include "Effect\Effects\Enum\EFFECT_ID.h"
 #include "Effect\Effects\Effects.h"
 
-class EffectManager :public Singleton<EffectManager>
+/// <summary>
+/// エフェクトを管理するクラス
+/// </summary>
+class EffectManager
 {
 public:
-	friend class Singleton<EffectManager>;
+	EffectManager() {};
 
 	~EffectManager() 
 	{
-		AllDelete();
+		AllRelease();
 	}
 
-	inline VOID CreateEffect(const D3DXVECTOR3& center, int effectId)
-	{
-		switch (effectId)
-		{
-		case EI_SCORE:
-			m_pEffects.push_back(static_cast<Effect*>(new GetScoreStarEffect(center)));
+	/// <summary>
+	/// エフェクトの追加
+	/// </summary>
+	/// <param name="pEffect">追加したいエフェクトのポインタ</param>
+	void AddEffect(Effect* pEffect);
 
-			break;
+	/// <summary>
+	/// エフェクトの更新
+	/// </summary>
+	void Update();
 
-		case EI_CLEAR:
-			m_pEffects.push_back(static_cast<Effect*>(new GetClearStarEffect(center)));
+	/// <summary>
+	/// エフェクトの描画
+	/// </summary>
+	void Render();
 
-			break;
-
-		case EI_DAMAGE:
-			m_pEffects.push_back(static_cast<Effect*>(new GetDamageStarEffect(center)));
-
-			break;
-
-		case EI_FLOWER_FALLING:
-			m_pEffects.push_back(static_cast<Effect*>(new FlowerFallingEffect()));
-
-			break;
-
-		default:
-			break;
-		}
-	}
-
-	inline VOID Update()
-	{
-		if (m_pEffects.size() <= 0) return;
-
-		ReleaseEndEffects();
-
-		for (auto i : m_pEffects)
-		{
-			i->Update();
-		}
-	}
-
-	inline VOID Render()
-	{
-		if (m_pEffects.size() <= 0) return;
-
-		for (auto i : m_pEffects)
-		{
-			i->Render();
-		}
-	}
-
-	inline VOID AllDelete()
-	{
-		for (auto i : m_pEffects)
-		{
-			delete i;
-		}
-
-		m_pEffects.clear();
-		m_pEffects.shrink_to_fit();
-	}
+	/// <summary>
+	/// 全てのエフェクトの開放
+	/// </summary>
+	void AllRelease();
 
 private:
-	EffectManager() 
-	{
-		m_pEffects.push_back(static_cast<Effect*>(new MouseCursorEffect()));
-	}
-
-	inline VOID ReleaseEndEffects()
-	{
-		if (m_pEffects.size() <= 0) return;
-
-		for (int i = static_cast<int>(m_pEffects.size() - 1); i >= 0; --i)
-		{
-			if (m_pEffects[i]->Ends())
-			{
-				delete m_pEffects[i];
-
-				std::vector<Effect*>::iterator thisIndexItr = m_pEffects.begin() + i;
-
-				m_pEffects.erase(thisIndexItr);
-			}
-		}
-	}
-
-	const LPDIRECT3DDEVICE9& m_pDX_GRAPHIC_DEVICE = nullptr;
+	/// <summary>
+	/// 終了したエフェクトの開放
+	/// </summary>
+	void ReleaseEndEffects();
 
 	std::vector<Effect*> m_pEffects;
 };
