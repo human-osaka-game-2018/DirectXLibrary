2e9372d4c898f44e28e603e3c32c5737dc9ac018
ToshiyaMatsuoka <triger.1306@gmail.com>
2019-02-21 10:50:22 +0900

    [issue #10] Xinputデバイス,その管理クラスの作成

---
+++ DirectXLibrary/GameLib/XInputManager/XInput/XinputDevice.cpp
@@ -0,0 +1,335 @@
+﻿/// <summary>
+/// XinputDevice関連処理
+/// </summary>
+/**
+* @author Toshiya Matsuoka
+*/
+
+#include "XinputDevice.h"
+
+using namespace Xinput;
+
+void XinputDevice::GetControl()
+{
+	XINPUT_INFO = XInputGetState(ControlerNum, &m_Xinput);
+}
+
+PADSTATE XinputDevice::GetButton(ButtonIndex index)
+{
+	return m_PadState[index];
+}
+
+void XinputDevice::BottonCheck() {
+
+	CheckButtonState(XINPUT_GAMEPAD_A, ButtonA);
+	CheckButtonState(XINPUT_GAMEPAD_B, ButtonB);
+	CheckButtonState(XINPUT_GAMEPAD_X, ButtonX);
+	CheckButtonState(XINPUT_GAMEPAD_Y, ButtonY);
+	CheckButtonState(XINPUT_GAMEPAD_START, ButtonStart);
+	CheckButtonState(XINPUT_GAMEPAD_BACK, ButtonBack);
+	CheckButtonState(XINPUT_GAMEPAD_LEFT_SHOULDER, ButtonLB);
+	CheckButtonState(XINPUT_GAMEPAD_RIGHT_SHOULDER, ButtonRB);
+	CheckButtonState(XINPUT_GAMEPAD_DPAD_UP, ButtonUP);
+	CheckButtonState(XINPUT_GAMEPAD_DPAD_DOWN, ButtonDOWN);
+	CheckButtonState(XINPUT_GAMEPAD_DPAD_LEFT, ButtonLEFT);
+	CheckButtonState(XINPUT_GAMEPAD_DPAD_RIGHT, ButtonRIGHT);
+	CheckButtonState(XINPUT_GAMEPAD_LEFT_THUMB, ButtonLeftThumb);
+	CheckButtonState(XINPUT_GAMEPAD_RIGHT_THUMB, ButtonRightThumb);
+
+	AnalogLStateDivide(ANALOGUP);
+	AnalogRStateDivide(ANALOGUP);
+	AnalogLStateDivide(ANALOGDOWN);
+	AnalogRStateDivide(ANALOGDOWN);
+	AnalogLStateDivide(ANALOGLEFT);
+	AnalogRStateDivide(ANALOGLEFT);
+	AnalogLStateDivide(ANALOGRIGHT);
+	AnalogRStateDivide(ANALOGRIGHT);
+
+	TriggerRStateDivide();
+	TriggerLStateDivide();
+}
+
+XinputDevice::XinputDevice(PLAYER_NUM number):ControlerNum(number) 
+{
+}
+
+void XinputDevice::DeviceUpdate() {
+
+	GetControl();
+	BottonCheck();
+}
+
+void XinputDevice::CheckButtonState(WORD ButtomID, ButtonIndex ButtomIndex)
+{
+	if (m_Xinput.Gamepad.wButtons & ButtomID)
+	{
+		if (m_PadOldState[ButtomIndex] == PadOn)
+		{
+			m_PadState[ButtomIndex] = PadOn;
+		}
+		else
+		{
+			m_PadState[ButtomIndex] = PadPush;
+		}
+
+		m_PadOldState[ButtomIndex] = PadOn;
+	}
+	else
+	{
+		if (m_PadOldState[ButtomIndex] == PadOn)
+		{
+			m_PadState[ButtomIndex] = PadRelease;
+		}
+		else
+		{
+			m_PadState[ButtomIndex] = PadOff;
+		}
+		m_PadOldState[ButtomIndex] = PadOff;
+	}
+}
+
+int XinputDevice::GetAnalogTrigger(AnalogTrigger Trigger)
+{
+	switch (Trigger)
+	{
+	case LEFT_TRIGGER:
+		return m_Xinput.Gamepad.bLeftTrigger;
+	case RIGHT_TRIGGER:
+		return m_Xinput.Gamepad.bRightTrigger;
+	}
+	return 0;
+}
+
+bool XinputDevice::GetAnalogL(Analog AnalogState)
+{
+	switch (AnalogState)
+	{
+	case ANALOGUP:
+		if (GetAnalogLValue(ANALOG_Y) > INCLINATION_THRESHOLD_VALUE)
+		{
+			return true;
+		}
+		break;
+	case ANALOGDOWN:
+		if (GetAnalogLValue(ANALOG_Y) < -INCLINATION_THRESHOLD_VALUE)
+		{
+			return true;
+		}
+		break;
+	case ANALOGRIGHT:
+		if (GetAnalogLValue(ANALOG_X) > INCLINATION_THRESHOLD_VALUE)
+		{
+			return true;
+		}
+		break;
+	case ANALOGLEFT:
+		if (GetAnalogLValue(ANALOG_X) < -INCLINATION_THRESHOLD_VALUE)
+		{
+			return true;
+		}
+		break;
+	default:
+		return false;
+	}
+	return false;
+}
+
+bool XinputDevice::GetAnalogR(Analog AnalogState)
+{
+	switch (AnalogState)
+	{
+	case ANALOGUP:
+		if (GetAnalogRValue(ANALOG_Y) > INCLINATION_THRESHOLD_VALUE)
+		{
+			return true;
+		}
+		break;
+	case ANALOGDOWN:
+		if (GetAnalogRValue(ANALOG_Y) < -INCLINATION_THRESHOLD_VALUE)
+		{
+			return true;
+		}
+		break;
+	case ANALOGRIGHT:
+		if (GetAnalogRValue(ANALOG_X) > INCLINATION_THRESHOLD_VALUE)
+		{
+			return true;
+		}
+		break;
+	case ANALOGLEFT:
+		if (GetAnalogRValue(ANALOG_X) < -INCLINATION_THRESHOLD_VALUE)
+		{
+			return true;
+		}
+		break;
+	default:
+		return false;
+	}
+	return false;
+}
+
+int XinputDevice::GetAnalogLValue(AnalogAxis AnalogState)
+{
+	switch (AnalogState)
+	{
+	case ANALOG_Y:
+		return m_Xinput.Gamepad.sThumbLY;
+		break;
+	case ANALOG_X:
+		return m_Xinput.Gamepad.sThumbLX;
+		break;
+	default:
+		return 0;
+	}
+}
+
+int XinputDevice::GetAnalogRValue(AnalogAxis AnalogState)
+{
+	switch (AnalogState)
+	{
+	case ANALOG_Y:
+		return m_Xinput.Gamepad.sThumbRY;
+		break;
+	case ANALOG_X:
+		return m_Xinput.Gamepad.sThumbRX;
+		break;
+	default:
+		return 0;
+	}
+}
+
+PADSTATE XinputDevice::GetAnalogLState(Analog AnalogState)
+{
+	return m_AnalogLState[AnalogState];
+}
+
+PADSTATE XinputDevice::GetAnalogRState(Analog AnalogState)
+{
+	return m_AnalogRState[AnalogState];
+}
+
+PADSTATE XinputDevice::GetTriggerRState()
+{
+	return m_TriggerRState;
+}
+
+PADSTATE XinputDevice::GetTriggerLState()
+{
+	return m_TriggerLState;
+}
+
+void XinputDevice::AnalogRStateDivide(Analog AnalogState) {
+	if (GetAnalogR(AnalogState))
+	{
+		if (m_AnalogROldState[AnalogState] == PadOn)
+		{
+			m_AnalogRState[AnalogState] = PadOn;
+		}
+		else
+		{
+			m_AnalogRState[AnalogState] = PadPush;
+		}
+		m_AnalogROldState[AnalogState] = PadOn;
+	}
+	else
+	{
+		if (m_AnalogROldState[AnalogState] == PadOn)
+		{
+			m_AnalogRState[AnalogState] = PadRelease;
+		}
+		else
+		{
+			m_AnalogRState[AnalogState] = PadOff;
+		}
+		m_AnalogROldState[AnalogState] = PadOff;
+	}
+}
+
+void XinputDevice::AnalogLStateDivide(Analog AnalogState) {
+	if (GetAnalogL(AnalogState))
+	{
+		if (m_AnalogLOldState[AnalogState] == PadOn)
+		{
+			m_AnalogLState[AnalogState] = PadOn;
+		}
+		else
+		{
+			m_AnalogLState[AnalogState] = PadPush;
+		}
+		m_AnalogLOldState[AnalogState] = PadOn;
+	}
+	else
+	{
+		if (m_AnalogLOldState[AnalogState] == PadOn)
+		{
+			m_AnalogLState[AnalogState] = PadRelease;
+		}
+		else
+		{
+			m_AnalogLState[AnalogState] = PadOff;
+		}
+		m_AnalogLOldState[AnalogState] = PadOff;
+	}
+}
+
+void XinputDevice::TriggerRStateDivide() {
+	if (GetAnalogTrigger(RIGHT_TRIGGER))
+	{
+		if (m_TriggerROldState == PadOn)
+		{
+			m_TriggerRState = PadOn;
+		}
+		else
+		{
+			m_TriggerRState = PadPush;
+		}
+		m_TriggerROldState = PadOn;
+	}
+	else
+	{
+		if (m_TriggerROldState == PadOn)
+		{
+			m_TriggerRState = PadRelease;
+		}
+		else
+		{
+			m_TriggerRState = PadOff;
+		}
+		m_TriggerROldState = PadOff;
+	}
+}
+void XinputDevice::TriggerLStateDivide() {
+	if (GetAnalogTrigger(LEFT_TRIGGER))
+	{
+		if (m_TriggerLOldState == PadOn)
+		{
+			m_TriggerLState = PadOn;
+		}
+		else
+		{
+			m_TriggerLState = PadPush;
+		}
+		m_TriggerLOldState = PadOn;
+	}
+	else
+	{
+		if (m_TriggerLOldState == PadOn)
+		{
+			m_TriggerLState = PadRelease;
+		}
+		else
+		{
+			m_TriggerLState = PadOff;
+		}
+		m_TriggerLOldState = PadOff;
+	}
+}
+
+void XinputDevice::RunVibration(unsigned int LeftValue, unsigned int RightValue) {
+	XINPUT_VIBRATION vibration;
+	ZeroMemory(&vibration, sizeof(XINPUT_VIBRATION));
+	vibration.wLeftMotorSpeed = LeftValue;
+	vibration.wRightMotorSpeed = RightValue;
+	XInputSetState(ControlerNum, &vibration);
+}
+
---
+++ DirectXLibrary/GameLib/XInputManager/XInput/XinputDevice.h
@@ -0,0 +1,277 @@
+﻿#ifndef XINPUT_DEVICE_H
+#define XINPUT_DEVICE_H
+/// <summary>
+/// XinputDevice関連処理
+/// </summary>
+/**
+* @author Toshiya Matsuoka
+*/
+#include <Windows.h>
+#include<Xinput.h>
+/// <summary>
+/// Xinputでの列挙
+/// </summary>
+namespace Xinput {
+	/// <summary>
+	/// コントローラ番号
+	/// </summary>
+	enum PLAYER_NUM
+	{
+		PLAYER_01,
+		PLAYER_02,
+		PLAYER_03,
+		PLAYER_04,
+		PLAYER_MAX
+	};
+
+	/// <summary>
+	/// ボタンの押下状態
+	/// </summary>
+	enum PADSTATE
+	{
+		//! 押されていない
+		PadOff,
+		//! 押されている
+		PadOn,
+		//! 押された
+		PadPush,
+		//! 離された
+		PadRelease
+	};
+	/// <summary>
+	/// パッドのボタン番号
+	/// </summary>
+	enum ButtonIndex
+	{
+		ButtonA,
+		ButtonB,
+		ButtonX,
+		ButtonY,
+		//! R1ボタン
+		ButtonRB,
+		//! L1ボタン
+		ButtonLB,
+		ButtonStart,
+		ButtonBack,
+		ButtonUP,
+		ButtonDOWN,
+		ButtonLEFT,
+		ButtonRIGHT,
+		//!L3ボタン
+		ButtonLeftThumb,
+		//!R3ボタン
+		ButtonRightThumb,
+		ButtomIndexMAX
+	};
+
+	/// <summary>
+	/// アナログスティックの傾き方向
+	/// </summary>
+	enum Analog
+	{
+		ANALOGUP,
+		ANALOGDOWN,
+		ANALOGLEFT,
+		ANALOGRIGHT,
+		ANALOGMAX
+	};
+	/// <summary>
+	/// アナログスティックの傾き軸
+	/// </summary>
+	enum AnalogAxis
+	{
+		ANALOG_X,
+		ANALOG_Y,
+	};
+	/// <summary>
+	/// 左右のトリガー識別
+	/// </summary>
+	enum AnalogTrigger
+	{
+		LEFT_TRIGGER,
+		RIGHT_TRIGGER
+	};
+};
+/// <summary>
+/// XinputPAD
+/// </summary>
+class XinputDevice {
+	
+public:
+	
+	explicit XinputDevice(Xinput::PLAYER_NUM number);
+
+	~XinputDevice() {};
+
+	/// <summary>
+	/// 接続状態の取得
+	/// </summary>
+	DWORD GetInfo() {
+		return XINPUT_INFO;
+	}
+
+	/// <summary>
+	/// GetControlとBottonCheckを纏めて行う
+	/// </summary>
+	/// <seealso cref="GetControl"/>
+	/// <seealso cref="BottonCheck"/>
+	void DeviceUpdate();
+
+	/// <summary>
+	/// XinputDeviceでゲームパッドのトリガーの入力取得
+	/// </summary>
+	/// <param name="Trigger">トリガーの左右認識番号</param>
+	/// <returns>0～255の値、押してなければ0</returns>
+	/// <seealso cref="Xinput::AnalogTrigger"/>
+	int GetAnalogTrigger(Xinput::AnalogTrigger Trigger);
+
+	/// <summary>
+	/// XinputDeviceでゲームパッドの左アナログスティック入力取得
+	/// </summary>
+	/// <param name="AnalogState">スティックの方向け先番号</param>
+	/// <returns>傾いていればTrue、そうでなければFalse</returns>
+	/// <seealso cref="Xinput::Analog"/>
+	bool GetAnalogL(Xinput::Analog AnalogState);
+
+	/// <summary>
+	/// XinputDeviceでゲームパッドの右アナログスティック入力取得
+	/// </summary>
+	/// <param name="AnalogState">スティックの方向け先番号</param>
+	/// <returns>傾いていればTrue、そうでなければFalse</returns>
+	/// <seealso cref="Xinput::Analog"/>
+	bool GetAnalogR(Xinput::Analog AnalogState);
+
+	/// <summary>
+	/// XinputDeviceでゲームパッドの左アナログスティック入力取得
+	/// </summary>
+	/// <param name="AnalogState">スティックの方向け先番号</param>
+	/// <returns>傾き具合の数値　MAX＝32767　MIN＝-32768</returns>
+	/// <seealso cref="Xinput::Analog"/>
+	int GetAnalogLValue(Xinput::AnalogAxis AnalogState);
+
+	/// <summary>
+	/// XinputDeviceでゲームパッドの右アナログスティック入力取得
+	/// </summary>
+	/// <param name="AnalogState">スティックの方向け先番号</param>
+	/// <returns>傾き具合の数値　MAX＝32767　MIN＝-32768</returns>
+	/// <seealso cref="Xinput::Analog"/>
+	int GetAnalogRValue(Xinput::AnalogAxis AnalogState);
+
+	/// <summary>
+	/// XinputDeviceでゲームパッドの左アナログスティック入力状態取得
+	/// </summary>
+	/// <param name="AnalogState">スティックの方向け先番号</param>
+	/// <returns>方向け状態</returns>
+	/// <seealso cref="Xinput::Analog"/>
+	/// <seealso cref="Xinput::PADSTATE"/>
+	Xinput::PADSTATE GetAnalogLState(Xinput::Analog AnalogState);
+
+	/// <summary>
+	/// XinputDeviceでゲームパッドの左アナログスティック入力状態取得
+	/// </summary>
+	/// <param name="AnalogState">スティックの方向け先番号</param>
+	/// <returns>方向け状態</returns>
+	/// <seealso cref="Xinput::Analog"/>
+	/// <seealso cref="Xinput::PADSTATE"/>
+	Xinput::PADSTATE GetAnalogRState(Xinput::Analog AnalogState);
+
+	/// <summary>
+	/// XinputDeviceでゲームパッドの右トリガー入力状態取得
+	/// </summary>
+	/// <returns>押下状態</returns>
+	/// <seealso cref="Xinput::PADSTATE"/>
+	Xinput::PADSTATE GetTriggerRState();
+
+	/// <summary>
+	/// XinputDeviceでゲームパッドの左トリガー入力状態取得
+	/// </summary>
+	/// <returns>押下状態</returns>
+	/// <seealso cref="Xinput::PADSTATE"/>
+	Xinput::PADSTATE GetTriggerLState();
+
+	/// <summary>
+	/// 左右のバイブレーションモーターを動作させる
+	/// </summary>
+	/// <param name="LeftValue">バイブレーション値 MAX＝65535　MIN＝0</param>
+	/// <param name="RightValue">バイブレーション値 MAX＝65535　MIN＝0</param>
+	/// <remarks>右は高周波モーター、左は低周波モーター</remarks>	
+	void RunVibration(unsigned int LeftValue = 0, unsigned int RightValue = 0);
+
+private:
+	const Xinput::PLAYER_NUM ControlerNum = Xinput::PLAYER_01;
+	XINPUT_STATE m_Xinput = { NULL };
+
+	Xinput::PADSTATE m_PadState[Xinput::ButtomIndexMAX] = { Xinput::PadOff };
+	Xinput::PADSTATE m_PadOldState[Xinput::ButtomIndexMAX] = { Xinput::PadOff };
+
+	Xinput::PADSTATE m_AnalogROldState[Xinput::ANALOGMAX] = { Xinput::PadOff };
+	Xinput::PADSTATE m_AnalogRState[Xinput::ANALOGMAX] = { Xinput::PadOff };
+
+	Xinput::PADSTATE m_AnalogLOldState[Xinput::ANALOGMAX] = { Xinput::PadOff };
+	Xinput::PADSTATE m_AnalogLState[Xinput::ANALOGMAX] = { Xinput::PadOff };
+	
+	Xinput::PADSTATE m_TriggerROldState = Xinput::PadOff;
+	Xinput::PADSTATE m_TriggerRState = Xinput::PadOff;
+
+	Xinput::PADSTATE m_TriggerLOldState = Xinput::PadOff;
+	Xinput::PADSTATE m_TriggerLState = Xinput::PadOff;
+
+	//! デバイスの状態確認用
+	DWORD XINPUT_INFO = -1;
+	
+	const int INCLINATION_THRESHOLD_VALUE = 65535 / 4;
+	
+	/// <summary>
+	/// XinputDeviceでゲームパッドデバイスの取得
+	/// </summary>
+	void GetControl();
+
+	/// <summary>
+	/// XinputDeviceでゲームパッドの入力取得
+	/// </summary>
+	/// <param name="index">取得したいボタンの配列番号</param>
+	/// <returns>PADSTATEの値</returns>
+	/// <seealso cref="Xinput::PADSTATE"/>
+	Xinput::PADSTATE GetButton(Xinput::ButtonIndex index);
+
+	/// <summary>
+	/// 全てのボタンに対してCheckButtonStateを行う
+	/// </summary>
+	/// <seealso cref="CheckButtonState"/>
+	void BottonCheck();
+
+	/// <summary>
+	/// XinputDeviceでゲームパッドの左アナログスティック入力状態振り分け
+	/// </summary>
+	/// <param name="AnalogState">入力状態の欲しい方向</param>
+	/// <seealso cref="Xinput::Analog"/>
+	void AnalogLStateDivide(Xinput::Analog AnalogState);
+
+	/// <summary>
+	/// XinputDeviceでゲームパッドの右アナログスティック入力状態振り分け
+	/// </summary>
+	/// <param name="AnalogState">入力状態の欲しい方向</param>
+	/// <seealso cref="Xinput::Analog"/>
+	void AnalogRStateDivide(Xinput::Analog AnalogState);
+
+	/// <summary>
+	/// XinputDeviceでゲームパッドの右トリガー入力状態振り分け
+	/// </summary>
+	void TriggerRStateDivide();
+
+	/// <summary>
+	/// XinputDeviceでゲームパッドの左トリガー入力状態振り分け
+	/// </summary>
+	void TriggerLStateDivide();
+
+	/// <summary>
+	/// Dinputでキーボードの状態取得
+	/// </summary>
+	/// <param name="ButtomID">取得したいXInputボタン番号</param>
+	/// <param name="ButtomIndex">取得したいボタンの配列番号</param>
+	/// <seealso cref="Xinput::ButtonIndex"/>
+	void CheckButtonState(WORD ButtomID, Xinput::ButtonIndex ButtomIndex);
+
+
+};
+#endif
